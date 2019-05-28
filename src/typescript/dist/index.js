8d11dd553d85465b80ebacddb23e2aaaa8418d2a
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-05-28 08:48:01 -0400

    Transpile TypeScript

---
+++ src/typescript/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+window.__COMPONENTS__ = window.__COMPONENTS__ || {};
+window.__COMPONENTS__.SimpleComponent = require('./simple');
+window.__COMPONENTS__.ComplexComponent = require('./complex');
