33f29269a7faf6d217ca968e4ca1cd4e171ae9fb
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-05-28 11:58:26 -0400

    chore(webpack): Update TypeScript config file to handle 'mode'

--- src/typescript/webpack.config.js
+++ src/typescript/webpack.config.js
@@ -1,10 +1,14 @@
+const PRODUCTION = 'production';
+const DEVELOPMENT = 'development';
+const MODES = [PRODUCTION, DEVELOPMENT];
+
 module.exports = {
+  mode: MODES.includes(process.env.NODE_ENV) ? process.env.NODE_ENV : DEVELOPMENT,
   entry: `${__dirname}/dist`,
   output: {
-    filename: 'bundle.min.js',
+    filename: process.env.NODE_ENV === PRODUCTION ? 'bundle.min.js' : 'bundle.js',
     path: `${__dirname}/dist`,
   },
   module: {},
   plugins: [],
 };
-
